b82a5c772b1bc40ce94d55854345784670aaf8b5
Nikhil Adhikari <nikhiladhikari1@gmail.com>
2026-05-03 16:03:13 +0200

    feat: add check_usable_rows validator - fatal if PIF/CHGOFF below threshold

--- src/data/validator.py
+++ src/data/validator.py
@@ -35,6 +35,54 @@ def check_columns(df: pd.DataFrame) -> dict:
         
         
     }
+
+
+def check_usable_rows(df: pd.DataFrame)-> dict:
+    """
+    Check that the number of pif and chgoff loan_status_values are above the threshold.
+
+    args:
+        df (pd.DataFrame): The dataframe of the dataset 
+
+    Returns:
+        dict with status ('pass' or 'fail'). On both cases, includes 
+            'total row count' 'number of rows with "pif" and "chgoff" loan_status'
+    
+    
+    """
+
+    ABSOLUTE_THRESHOLD = 50_000
+    PERCENT_THRESHOLD = 0.12
+
+    total_rows = len(df)
+
+    
+
+    usable_rows = df["loanstatus"].isin(["pif", "chgoff"]).sum()
+    usable_pct = usable_rows / total_rows
+
+    if usable_pct < PERCENT_THRESHOLD or usable_rows < ABSOLUTE_THRESHOLD:
+        return {
+            "status": "fail",
+            "details": {
+                "total_rows": total_rows,
+                "usable_rows": usable_rows,
+                "usable_pct": round(usable_pct, 4),
+
+            }
+            
+
+        }
+
+    return {
+        "status": "pass",
+        "details": {
+            "total_rows": total_rows,
+            "usable_rows": usable_rows,
+            "usable_pct": round(usable_pct, 4),
+        }
+        
+    }
     
 
 
